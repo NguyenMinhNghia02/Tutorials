99c8b34f13dfb380a74b6eddfdb9f05e3b65fd7e
NguyenMinhNghia02 <nghia.nm204594@sis.hust.edu.vn>
2023-12-18 17:20:59 +0700

    update

--- split_to_train_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script creates a train/test split
-echo "WARNING: script testset has ETA of ~40 min"
-cd ../../recsys-dataset || exit
-echo $(pwd)
-python -m src.testset \
-  --train-set ../otto-recommender-system/train.jsonl \
-  --days 7 \
-  --output-path ../otto-recommender/data/train-test/
